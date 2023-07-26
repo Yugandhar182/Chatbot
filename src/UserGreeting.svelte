6f7f773a106a04945968c725074069b3ae421d12
Yugandhar182 <yugandhar@recruitly.io>
2023-07-26 16:22:50 +0530

    .

--- src/UserGreeting.svelte
+++ src/UserGreeting.svelte
@@ -2,17 +2,8 @@
     export let userDisplayName;
   </script>
   
-  <div class="user-name-container">
-    <h2>Welcome, {userDisplayName}!</h2>
-  </div>
   
-  <style>
-    .user-name-container {
-      padding: 10px;
-      background-color: #f9f9f9;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      margin-bottom: 10px;
-    }
-  </style>
+    <h2>Welcome, {userDisplayName}!</h2>
+  
+ 
   
